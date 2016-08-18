145eafd89e01c91fdffd7b6ebee3c1f5717d4674
Luis Vicente <lfvicente@gmail.com>
2016-08-18 13:45:26 +0200

    Busquedas de usuarios(a modo de prueba)

--- app/models/t_usuario.rb
+++ app/models/t_usuario.rb
@@ -1,3 +1,6 @@
 class TUsuario < ActiveRecord::Base
 	self.table_name ="geslico.dbo.TUsuarios"
+	scope :cCodUsuario, -> (cCodUsuario) { where cCodUsuario: cCodUsuario }
+	scope :cNombre, -> (cNombre) { where("cNombre like ?", "#{cNombre}%")}
+	scope :cClave, -> (cClave) { where cClave: cCodUsuario }
 end
