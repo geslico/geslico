48cec18db68d26da45d757937c03290f25a9b522
sandrafg <xndmvl@gmail.com>
2018-01-31 09:57:24 +0100

    Visualización en rejilla de las líneas VLan

--- app/models/lin_datos_vpn_vlan.rb
+++ app/models/lin_datos_vpn_vlan.rb
@@ -2,8 +2,11 @@ class LinDatosVpnVlan < ApplicationRecord
 
 	self.table_name ="geslico.dbo.TLinDatosVPNVLan"
 	self.primary_keys = :nCodLinDatos,:nIdVPN,:nIdVLan
+	
 
 	belongs_to :lin_dato , :foreign_key => "nCodLinDatos"
 	belongs_to :vpn_vlan , :foreign_key => [:nIdVPN, :nIdVLan]
 	
+	scope :vlanEstandar,     -> { where("nCodLinDatos = 410001")}
+
 end
