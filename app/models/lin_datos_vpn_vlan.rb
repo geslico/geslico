5ec578575e65ea9bede55bc3a5e7af0444bb802e
sandrafg <xndmvl@gmail.com>
2018-01-31 10:36:34 +0100

    solucionado la visualización de los distintos tipos de Vlan en función del cabecera que tenga asignada la línea de datos

--- app/models/lin_datos_vpn_vlan.rb
+++ app/models/lin_datos_vpn_vlan.rb
@@ -3,10 +3,9 @@ class LinDatosVpnVlan < ApplicationRecord
 	self.table_name ="geslico.dbo.TLinDatosVPNVLan"
 	self.primary_keys = :nCodLinDatos,:nIdVPN,:nIdVLan
 	
-
 	belongs_to :lin_dato , :foreign_key => "nCodLinDatos"
 	belongs_to :vpn_vlan , :foreign_key => [:nIdVPN, :nIdVLan]
 	
-	scope :vlanEstandar,     -> { where("nCodLinDatos = 410001")}
+	scope :cabecera, ->(nLinea) {where(:nCodLinDatos => nLinea)}
 
 end
